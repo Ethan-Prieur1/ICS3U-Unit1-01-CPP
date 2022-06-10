511f43c464f4280f00d7b7d863f5e61a27484e3e
Ubuntu <ubuntu@ip-172-31-41-46.ec2.internal>
2022-06-10 17:57:07 +0000

    First Commit

---
+++ beginning.cpp
@@ -0,0 +1,16 @@
+// Copyright (c) 2022 Ethan Prieur All rights reserved
+//
+// Created by: Ethan Prieur
+// Created on: June 2022
+// This program has 3 print statements, that's it
+
+#include <iostream> 
+
+int main() {
+// this is the main function
+
+std::cout << "Ethan" << std::endl;
+std::cout << "Ontario" << std::endl;
+std::cout << "Canada" << std::endl;
+
+}
